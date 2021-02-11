f0d684a4eb6284a71271e9ea8425aaf7b9e2939f
Christopher Lainson <chris.lainson@gmail.com>
2021-02-11 17:22:29 -0500

    add gitignore

---
+++ forms.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<Body>
+<H1>PHP Form</h1>
+<form>
+    <label for="tname">Team Name</label>
+    <input type="text" id="tname" Name="tname">
+    <br>
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="fname">
+    <br>
+    <label for="lname">Last Name</label>
+    <input type="text" id=Lname name="lname">
+    <br>
+    <label for="position">Position</label>
+    <input type="text" id=position name="position">
+    <br>
